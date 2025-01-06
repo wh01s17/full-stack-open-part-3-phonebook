1f4443f2ef5a3922ce4b1279ce2d3dd775892dc4
wh01s17 <jor.perezo@hotmail.com>
2025-01-06 19:23:28 -0300

    exercise_3.7 and 3.8

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
 app.use(express.json())
+// app.use(morgan('tiny'))
+
+morgan.token('body', (req, res) => {
+    return JSON.stringify(req.body)
+})
+
+app.use(morgan(
+    ':method :url :status :res[content-length] - :response-time ms :body'
+))
+
 
 let persons = [
     {
